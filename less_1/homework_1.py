a9279dcaa968bf94b58095d7a4142fe320c31df0
yevhenii_vuichak <vuychak.eugene@icloud.com>
2026-08-24 20:32:55 +0200

    homework task 5

--- less_1/homework_1.py
+++ less_1/homework_1.py
@@ -22,18 +22,25 @@
 # print(b)
 
 # Task 4
-x = y = z = 100
-print(x)
-print(y)
-print(z)
-print(id(x))
-print(id(y))
-print(id(z))
+# x = y = z = 100
+# print(x)
+# print(y)
+# print(z)
+# print(id(x))
+# print(id(y))
+# print(id(z))
+#
+# x, y, z = 99, 200, 300
+# print(x)
+# print(y)
+# print(z)
+# print(id(x))
+# print(id(y))
+# print(id(z))
 
-x, y, z = 99, 200, 300
-print(x)
-print(y)
-print(z)
-print(id(x))
-print(id(y))
-print(id(z))
+# Task 5
+a = 5
+b = 10
+print(a, " ", b)
+a, b = b, a
+print(a, " ", b)
