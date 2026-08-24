a8fde9ca99b3e51032fc29a37e58125c8d36e4e4
yevhenii_vuichak <vuychak.eugene@icloud.com>
2026-08-24 20:23:36 +0200

    homework task 3

--- less_1/homework_1.py
+++ less_1/homework_1.py
@@ -6,9 +6,17 @@
 # print(name,"  ",age," ", height)
 
 # Task 2
-x = 10
-print(x)
-x = 25.5
-print(x)
-x = "Python"
-print(x)
+# x = 10
+# print(x)
+# x = 25.5
+# print(x)
+# x = "Python"
+# print(x)
+
+
+# Task 3
+a = 7
+b = a
+a = 10
+print(a)
+print(b)
