c60cb3e5fff7fb07c362f7bb4a781b3b5b309058
yevhenii_vuichak <vuychak.eugene@icloud.com>
2026-08-24 20:20:44 +0200

    homework task 2

--- less_1/homework_1.py
+++ less_1/homework_1.py
@@ -1,4 +1,14 @@
-name = "Eugene"
-age = 65
-height = 1.80
-print(name,"  ",age," ", height)
+
+# Task 1
+# name = "Eugene"
+# age = 65
+# height = 1.80
+# print(name,"  ",age," ", height)
+
+# Task 2
+x = 10
+print(x)
+x = 25.5
+print(x)
+x = "Python"
+print(x)
