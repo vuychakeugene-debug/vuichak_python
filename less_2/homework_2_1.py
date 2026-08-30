b8b8c3cd73c3c5c03e9545416d08b842026287f4
yevhenii_vuichak <vuychak.eugene@icloud.com>
2026-08-30 11:41:46 +0200

    homework less_2_1

---
+++ less_2/homework_2_1.py
@@ -0,0 +1,53 @@
+#  Task 1
+# a = 3
+# b = 7
+#
+# x = 2.55
+# y = 12.122121
+#
+# print(a, b, x, y)
+# print(type(a), type(b), type(x), type(y))
+
+
+# # Task 2
+#
+# num1 = 23
+# num2 = 5
+#
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+
+# Task 3
+#
+# a = 10
+# b = 3
+#
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print((a * -1) / b)
+# print((a * -1)  // b)
+# print((a * -1)  % b)
+# print(a / (b * -1))
+# print(a // (b * -1))
+# print(a % (b * -1))
+
+# # Task 4
+# print(5 + 3 * 2)
+# print(5 + 3 * 2)
+# print( 10 / 2 ** 2)
+
+# Task 5
+#
+# count = 10
+# count += 5
+# count -= 3
+# count *= 2
+# count /= 4
+# print(int(count))
