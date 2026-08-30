a392873a873e689acd2f75fc7b3f03106b13195a
yevhenii_vuichak <vuychak.eugene@icloud.com>
2026-08-30 12:05:25 +0200

    homework less_2_2

---
+++ less_2/homework_2_2.py
@@ -0,0 +1,69 @@
+# Task 1
+#
+# s1 = "Python"
+# s2 = "Програмирование"
+#
+# print(s1)
+# print(s2)
+
+# s1 = """    Python
+#     Програмирование
+#
+#     """
+# print (s1)
+
+# s1 = " "
+# print(len(s1))
+
+# Task 2
+#
+# first_name = "Ivan"
+# second_name = "Petro"
+# full_name = first_name + " " + second_name
+# print(full_name)
+
+# Task 3
+#
+# s = "Age: "
+# age = 25
+#
+# print(s + str(age))
+
+# Task 4
+#
+# s = "xa"
+# print(s * 5)
+
+# Task 5
+#
+# s = "Hello World"
+# s1 = ""
+# print(len(s))
+# print(len(s1))
+
+#  Task 6
+
+# text = "I learn Python"
+# print("Python" in text)
+# print("Java" in text)
+
+# Task 7
+#
+# a = "gppleeeeeeee"
+# b = "ganana"
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+
+# Task 8
+#
+# print(ord(" "))
+# print(ord("?"))
+# print(ord("B"))
+# print(ord("5"))
+# print(ord("§"))
+
+
